465462d940c3a9e2b0a17d557dc52cbdc8d696d8
leiliangliang <leiliangliang@coship.com>
2015-06-29 16:40:06 +0800

    Android单元测试

---
+++ src/com/coship/androidtestcase/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.coship.androidtestcase.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.coship.androidtestcase.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
